da698068ed8c309ba1b99cc9f28a594d10a63d19
HP <thegame.mzh@gmail.com>
2020-11-18 15:00:33 +0500

    initial commit

--- app/src/main/java/home/service/appmanage/online/adminapp/fragments/ViewWorkerDetailsFragment.kt
+++ app/src/main/java/home/service/appmanage/online/adminapp/fragments/ViewWorkerDetailsFragment.kt
@@ -8,12 +8,20 @@ import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Response
+import com.android.volley.VolleyError
+import com.android.volley.toolbox.StringRequest
 import com.bumptech.glide.Glide
 import home.service.appmanage.online.adminapp.R
 import home.service.appmanage.online.adminapp.activities.PreviewActivity
 import home.service.appmanage.online.adminapp.models.Workers
+import home.service.appmanage.online.adminapp.utils.Constants.UPDATE_ACTIVE_URL
 import home.service.appmanage.online.adminapp.utils.Constants.UPLOAD_DIRECTORY
+import home.service.appmanage.online.adminapp.utils.RequestHandler
+import home.service.appmanage.online.adminapp.utils.SharedPrefUtils
 import kotlinx.android.synthetic.main.fragment_view_worker_details.view.*
+import org.json.JSONException
+import org.json.JSONObject
 import java.util.*
 
 
@@ -22,10 +30,14 @@ class ViewWorkerDetailsFragment : BaseFragment() {
     private var position: Int = 0
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        serviceList = requireArguments().getParcelableArrayList("typed")
-        position = requireArguments().getInt("position")
-        (requireActivity() as AppCompatActivity).supportActionBar?.title =
-            serviceList!![position].name
+        try {
+            serviceList = requireArguments().getParcelableArrayList("typed")
+            position = requireArguments().getInt("position")
+            (requireActivity() as AppCompatActivity).supportActionBar?.title =
+                serviceList!![position].name
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
 
     override fun onCreateView(
@@ -34,28 +46,33 @@ class ViewWorkerDetailsFragment : BaseFragment() {
     ): View? {
         // Inflate the layout for this fragment
         root = inflater.inflate(R.layout.fragment_view_worker_details, container, false)
-        serviceList = requireArguments().getParcelableArrayList("typed")
-        position = requireArguments().getInt("position")
-        Glide.with(requireActivity())
-            .load(UPLOAD_DIRECTORY + serviceList!![position].profilePic)
-            .into(root!!.imagePickProfile)
-        Glide.with(requireActivity())
-            .load(UPLOAD_DIRECTORY + serviceList!![position].cnicImage)
-            .into(root!!.imagePick)
-        root!!.name1.setText(serviceList!![position].name)
-        root!!.email1.setText(serviceList!![position].email)
-        root!!.number.setText(serviceList!![position].phoneNum)
-        root!!.cnic_number.setText(serviceList!![position].cnicNum)
+        try {
+            serviceList = requireArguments().getParcelableArrayList("typed")
+            position = requireArguments().getInt("position")
+            Glide.with(requireActivity())
+                .load(UPLOAD_DIRECTORY + serviceList!![position].profilePic)
+                .into(root!!.imagePickProfile)
+            Glide.with(requireActivity())
+                .load(UPLOAD_DIRECTORY + serviceList!![position].cnicImage)
+                .into(root!!.imagePick)
+            root!!.name1.setText(serviceList!![position].name)
+            root!!.email1.setText(serviceList!![position].email)
+            root!!.number.setText(serviceList!![position].phoneNum)
+            root!!.cnic_number.setText(serviceList!![position].cnicNum)
+
+            root!!.imageLayoutProfile.setOnClickListener {
+                openPreview(serviceList!![position].profilePic)
+            }
+            root!!.imageLayout.setOnClickListener {
+                openPreview(serviceList!![position].cnicImage)
+
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
         setSpinner(R.array.activate_Array, root!!.workerType)
         root!!.register.setOnClickListener {
-            updateData()
-        }
-        root!!.imageLayoutProfile.setOnClickListener {
-            openPreview(serviceList!![position].profilePic)
-        }
-        root!!.imageLayout.setOnClickListener {
-            openPreview(serviceList!![position].cnicImage)
-
+            updateData(serviceList?.get(position)!!)
         }
         return root
     }
@@ -66,11 +83,61 @@ class ViewWorkerDetailsFragment : BaseFragment() {
         startActivity(intent)
     }
 
-    private fun updateData() {
+    private fun updateData(workers: Workers) {
         val workerType: String = root!!.workerType.selectedItem.toString()
+        var isActivate = false
         if (workerType == "Choose") {
             showToast("Choose the Option")
         } else {
+            if (workerType == "Activate") {
+                isActivate = true
+            } else if (workerType == "Deactivate") {
+                isActivate = false
+            }
+            showDialog(getString(R.string.updating))
+            val stringRequest: StringRequest =
+                object : StringRequest(
+                    Method.POST,
+                    UPDATE_ACTIVE_URL,
+                    Response.Listener { response: String ->
+                        try {
+                            try {
+                                val response_data = JSONObject(response)
+                                if (response_data.getString("status") == "1") {
+                                    showToast(response_data.getString("data"))
+                                } else {
+                                    showToast(response_data.getString("data"))
+                                }
+                                hideDialog()
+                            } catch (e: JSONException) {
+                                e.printStackTrace()
+                                hideDialog()
+                            }
+                        } catch (e: Exception) {
+                            e.printStackTrace()
+                        }
+                    },
+                    Response.ErrorListener { error: VolleyError ->
+                        try {
+                            hideDialog()
+                            showToast(error.message!!)
+                        } catch (e: Exception) {
+                            e.printStackTrace()
+                        }
+                    }
+                ) {
+                    override fun getParams(): Map<String, String> {
+                        val params: MutableMap<String, String> =
+                            HashMap()
+                        params["wid"] = workers.wId
+                        params["isActivate"] = isActivate.toString()
+
+                        return params
+                    }
+                }
+
+
+            RequestHandler.getInstance(requireContext()).addToRequestQueue(stringRequest)
         }
     }
 
--- app/src/main/java/home/service/appmanage/online/adminapp/utils/Constants.kt
+++ app/src/main/java/home/service/appmanage/online/adminapp/utils/Constants.kt
@@ -2,9 +2,13 @@ package home.service.appmanage.online.adminapp.utils
 
 object Constants {
     const val TAGI = "AdminApp"
+
     const val ROOT_URL = "https://doormansolutions.com/"
+
+    //    const val ROOT_URL = "http://23.111.145.114/"
     const val UPLOAD_DIRECTORY = ROOT_URL + "uploads/"
     const val LOGIN_URL = ROOT_URL + "adminlogin.php"
     const val FETCH_WORKERS = ROOT_URL + "admin_fetch_wrkers.php"
     const val CHANGE_PASS_URL = ROOT_URL + "adminChangePassword.php"
+    const val UPDATE_ACTIVE_URL = ROOT_URL + "update_activate_worker.php"
 }
